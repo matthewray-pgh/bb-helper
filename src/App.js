4e60573a324d17d0fd8eb56319afd1e988725add
Matthew <mray@zoll.com>
2023-07-07 13:40:58 -0400

    feature: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Link
+} from "react-router-dom";
+
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div className="App">
+        <h1>Home</h1>
+        <Link to="/login">Login</Link>
+      </div>
+    ),
+  },
+  {
+    path: "login",
+    element: <Login />
+  }
+]);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
